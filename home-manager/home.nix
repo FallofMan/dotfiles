5e3da2b0428df45b5eb7fc00e31d65b3bb3772bd
FallofMan <7422589+FallofMan@users.noreply.github.com>
2024-03-27 16:04:57 -0700

    updates

---
+++ home-manager/home.nix
@@ -0,0 +1,91 @@
+# This is your home-manager configuration file
+# Use this to configure your home environment (it replaces ~/.config/nixpkgs/home.nix)
+{
+  inputs,
+  outputs,
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+
+  nixpkgs = {
+    # You can add overlays here
+    overlays = [
+      # Add overlays your own flake exports (from overlays and pkgs dir):
+      # outputs.overlays.additions
+      # outputs.overlays.modifications
+      # outputs.overlays.unstable-packages
+
+      # You can also add overlays exported from other flakes:
+      # neovim-nightly-overlay.overlays.default
+
+      # Or define it inline, for example:
+      # (final: prev: {
+      #   hi = final.hello.overrideAttrs (oldAttrs: {
+      #     patches = [ ./change-hello-to-hi.patch ];
+      #   });
+      # })
+    ];
+    # Configure your nixpkgs instance
+    config = {
+      # Disable if you don't want unfree packages
+      allowUnfree = true;
+    };
+  };
+
+  home = {
+    username = "collin";
+    homeDirectory = "/home/collin";
+  };
+
+  home.packages = with pkgs; [
+    firefox
+    kate
+    # reaper
+    # spotify
+    # sublime
+    transmission    
+  ];
+
+  wayland.windowManager.hyprland = {
+    enable = true;
+
+    settings = {
+      #TODO
+    };
+  };
+
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    enableAutosuggestions = true;
+    syntaxHighlighting.enable = true;    
+
+    shellAliases = {
+      ll = "ls -l";
+    };
+    history.size = 10000;
+
+    oh-my-zsh = {
+      enable = true;
+      plugins = [
+        "git"
+        "pip"
+        "tmux"
+        "rust"
+      ];
+      theme = "af-magic";
+    };
+  };
+
+  # Enable home-manager and git
+  programs.home-manager.enable = true;
+  programs.git.enable = true;
+
+  # Nicely reload system units when changing configs
+  systemd.user.startServices = "sd-switch";
+
+  # Add more home-manager configuration options as needed
+  home.stateVersion = "23.11";
+}
